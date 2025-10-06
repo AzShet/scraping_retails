f49a97c197b52cac9e7b876ccf849910108589d6
César Diego <diegoruelasflores@gmail.com>
2025-10-05 19:03:31 -0500

    extractor de html web

---
+++ extractor_html.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+# url de emrcadolibre (ofertas
+url = "https://www.plazavea.com.pe/frutas-y-verduras
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(url, headers=headers)
+
+# Verificar que la solicitud fue exitosa
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Guardar el HTML en un archivo para analizarlo mejor
+    with open("mercadolibre.html", "w", encoding="utf-8") as file:
+        file.write(soup.prettify())
+    print("Archivo 'mercadolibre.html' guardado. Ábrelo en un navegador o en un␣ editor de texto para inspeccionar la estructura.")
+else:
+    print(f"Error al acceder a la página: {response.status_code}")
