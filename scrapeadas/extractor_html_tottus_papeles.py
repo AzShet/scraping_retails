c67d4cedcaa813f4b1cdb0bcc1d899a673260bc5
César Diego <diegoruelasflores@gmail.com>
2025-10-12 18:54:22 -0500

    New features

--- scrapeadas/extractor_html_tottus_papeles.py
+++ scrapeadas/extractor_html_tottus_papeles.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # url de emrcadolibre (ofertas
-url = "https://www.plazavea.com.pe/frutas-y-verduras"
+url = "https://tottus.falabella.com.pe/tottus-pe/category/cat13920469/Papeles"
 headers = {"User-Agent": "Mozilla/5.0"}
 response = requests.get(url, headers=headers)
 
@@ -10,7 +10,7 @@ response = requests.get(url, headers=headers)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
     # Guardar el HTML en un archivo para analizarlo mejor
-    with open("plazavea.html", "w", encoding="utf-8") as file:
+    with open("tottus_papeles.html", "w", encoding="utf-8") as file:
         file.write(soup.prettify())
     print("Archivo 'mercadolibre.html' guardado. Ábrelo en un navegador o en un␣ editor de texto para inspeccionar la estructura.")
 else:
